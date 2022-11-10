7c8a890959ea1c32b3bc3c44c3aa95acaee8ef47
Nikki <nlim042@e.ntu.edu.sg>
2022-11-10 11:48:36 +0800

    cartItem name change

--- php/addcart.php
+++ php/addcart.php
@@ -1,24 +1,24 @@
 <?php
     include 'php/connectdb.php';
-    $sql = "SELECT quantity, `transaction`.`transactionId`, productId FROM `transaction` , `cart_items` WHERE `transaction`.`transactionId` = `cart_items`.`transactionId` and custId=$cid and `paid`=0";
+    $sql = "SELECT quantity, `transaction`.`transactionId`, productId FROM `transaction` , `cartItem` WHERE `transaction`.`transactionId` = `cartItem`.`transactionId` and custId=$cid and `paid`=0";
 
     $result = mysqli_query($conn, $sql);
     $quantity = !isset($_POST['quantity']) ? 1 : $_POST['quantity']; 
 
     if(mysqli_num_rows($result)>0) {
         $row = mysqli_fetch_assoc($result);
-        $sql = "UPDATE `cart_items` set quantity = quantity+$quantity WHERE `transactionId` = ".$row['transactionId']." and productId = $id";
+        $sql = "UPDATE `cartItem` set quantity = quantity+$quantity WHERE `transactionId` = ".$row['transactionId']." and productId = $id";
         mysqli_query($conn, $sql);
         if(mysqli_affected_rows($conn)>0)
             $res = "INPUTCART";
         else{   
-            $sql = "INSERT INTO `cart_items` (`transactionId`, productId, quantity) VALUES (".$row['transactionId'].", $id, $quantity)";
+            $sql = "INSERT INTO `cartItem` (`transactionId`, productId, quantity) VALUES (".$row['transactionId'].", $id, $quantity)";
             mysqli_query($conn, $sql);
             $res = "ADDTOCART";
         }
     } else {
         $sql = "INSERT INTO `transaction` (paid, custId) VALUES (0, $cid);";
-        $sql .= "INSERT INTO `cart_items` (`transactionId`, productId, quantity) VALUES (LAST_INSERT_ID(), $id, $quantity)"; //LAST_INSERT_ID() gives the transactionId inserted into transaction in the previous statement
+        $sql .= "INSERT INTO `cartItem` (transactionId, productId, quantity) VALUES (LAST_INSERT_ID(), $id, $quantity)"; //LAST_INSERT_ID() gives the transactionId inserted into transaction in the previous statement
         mysqli_multi_query($conn, $sql);
         $res = "ADDTOCART";
     }
